4fed0fdeae37bd2e7364be1c0bd375032856b91f
mimovnik <mimovnik@protonmail.com>
2024-03-24 19:46:25 +0100

    Generate macd

--- macd.py
+++ macd.py
@@ -16,47 +16,52 @@ def ema(data, n):
 
 df = pd.read_csv('wig20_2019-2024.csv')
 data = df['Zamkniecie'].tolist()
-data2 = range(1, 20)
-df2 = pd.DataFrame(data2)
+print("data len " + str(len(data)))
 
-ema12 = df2.rolling(window=12, min_periods=1).mean()
+ema12 = ema(data, 12)
+print("ema12 len " + str(len(ema12)))
+ema26 = ema(data, 26)
+print("ema26 len " + str(len(ema26)))
 
-myema12 = ema(data2, 12)
+macd = []
+for i in range(len(ema12)):
+    macd.append(ema12[i] - ema26[i])
 
-for i in range(0, len(myema12)):
-    print(str(i) + ". " + "e = " +
-          str(ema12[0][i]) + "     mye  = " + str(myema12[i]))
+signal = ema(macd, 9)
 
-print("ema12 size = " + str(len(ema12[0])))
-print("myema12 size = " + str(len(myema12)))
-exit(0)
+buy_points = [None]
+sell_points = [None]
+for i in range(1, len(macd)):
+    if macd[i] > signal[i] and macd[i - 1] < signal[i - 1]:
+        buy_points.append(macd[i])
+    else:
+        buy_points.append(None)
 
-macd = ema12 - ema26
+    if macd[i] < signal[i] and macd[i - 1] > signal[i - 1]:
+        sell_points.append(macd[i])
+    else:
+        sell_points.append(None)
 
-signal = macd.rolling(window=9, min_periods=1).mean()
+print("buy_points len " + str(len(buy_points)))
+print("sell_points len " + str(len(sell_points)))
 
-# buy_points = np.where((macd > signal) & (
-#     macd.shift(1) < signal.shift(1)), macd, np.nan)
-#
-# sell_points = np.where((macd < signal) & (
-#     macd.shift(1) > signal.shift(1)), macd, np.nan)
-
-
+indexes = list(range(len(data)))
 # Plotting
 plt.figure(figsize=(18, 8))
 
 plt.subplot(2, 1, 1)
-plt.plot(df['Zamkniecie'], label='Close Price', color='blue')
+plt.plot(df['Data'], data, label='Close Price', color='blue')
+plt.xticks(df['Data'][::100])
 plt.title('Close Price')
 plt.legend()
 
 plt.subplot(2, 1, 2)
 plt.plot(macd, label='MACD', color='blue')
 plt.plot(signal, label='SIGNAL', color='red')
-# plt.scatter(df.index, buy_points, color='green',
-#             marker='^', label='Buy Signal')
-# plt.scatter(df.index, sell_points, color='orange',
-#             marker='v', label='Sell Signal')
+plt.scatter(indexes, buy_points, color='green',
+            marker='^', label='Buy Signal')
+plt.scatter(indexes, sell_points, color='orange',
+            marker='v', label='Sell Signal')
 plt.title('MACD, SIGNAL and buy/sell signals')
 plt.legend()
 
